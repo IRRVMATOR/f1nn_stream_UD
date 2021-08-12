2e7be9470a79bab7f8011bf4d66fdb453b9ac8f1
IRRVMATOR <88548215+IRRVMATOR@users.noreply.github.com>
2021-08-12 19:01:51 +0200

    Testing twitter api
    Seems stable so far, now testing with twitter api connected

--- twitter_bot.py
+++ twitter_bot.py
@@ -16,17 +16,31 @@ import tweepy
 import select
 import smtplib, ssl
 
+
+x_twitter_connection = True
+x_send_live_notif = True
+x_email_tweets = True
+
 #system startup time
 startup_time = time.time()
 log_file = "log.txt"
 stream_ud = ("title","date","time")
+live_notif_timeout = 0
 
 def _init():
     log(f'Bot started at {datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)")}')
     log("###################################################################################")
+
+    #dumb banner on startup
+    try:
+        from pyfiglet import Figlet
+        custom_fig = Figlet(font='slant')
+        print(custom_fig.renderText('f1nn twitter bot'))
+    except:
+        log("Intstall the packet pyfiglet if you want a cool big text banner")
+    
     sock = connect_twtich_irc()
     last_irc_msg = startup_time
-    #api = connect_twitter_api()
 
     #store last time f3mmbot said anthing about stream
     last_update = ""
@@ -66,9 +80,16 @@ def _init():
 
             #check if f3mmbot message and cut it down a bit 
             msg = handle_raw_message(resp)
-            if msg:
+
+            #send live notif if more than 30min since last live notif
+            if x_send_live_notif and msg and "F1NN5TER just went live!" in msg and time.time()-live_notif_timeout > 1800:
+                send_tweet("F1NN just went live\nhttps://www.twitch.tv/f1nn5ter")
+                live_notif_timeout = time.time()
+
+            #found scedule message    
+            elif msg and "The next stream will be" in msg:
                 log(f'Received stream update:"{msg}"')
-                #check if stream info changed
+                #check if scedule changed
                 if not msg == last_update:
                     log('New stream update :)')
                     last_update = msg
@@ -89,8 +110,7 @@ def _init():
                 f.write(last_update)
             #parse parameter from f3mmbot msg and check for filtered words
             if parse_stream_ud_info(last_update[24:-22]) and check_filters():
-                send_mail(f'Title {stream_ud[0]}\nDate  {stream_ud[1]}\nTime  {stream_ud[2]}')
-                #api.update_status(last_update)
+                send_tweet(f'Title {stream_ud[0]}\nDate  {stream_ud[1]}\nTime  {stream_ud[2]} UK Time')
                 log("TWEET: "+ last_update)
             #regardless if tweet sent, mark as don't send again
             send_tweet_flag = False
@@ -107,22 +127,41 @@ def _init():
 
 #TWITTER
 ############################################################################
-def connect_twitter_api():
-    with open('twitter_token.txt') as f:
-        lines = f.readlines()
+def send_tweet(msg):
+    global x_twitter_connection
+    if x_email_tweets:
+        send_mail(msg)
+    if x_twitter_connection:
+        try:
+            #reading tokens
+            with open('twitter_token.txt') as f:
+                lines = f.readlines()
 
-    auth = tweepy.OAuthHandler(lines[0][:-1], lines[1][:-1])
-    auth.set_access_token(lines[2][:-1], lines[3][:-1])
-    api = tweepy.API(auth,wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+            #setting up api 
+            auth = tweepy.OAuthHandler(lines[0][:-1], lines[1][:-1])
+            auth.set_access_token(lines[2][:-1], lines[3][:-1])
+            api = tweepy.API(auth,wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-    try:
-        api.verify_credentials()
-        log("Twitter: Authentication OK")
-        return api
-    except:
-        log("Twitter: Error during authentication")
+            #check api connection
+            try:
+                api.verify_credentials()
+                log("Twitter: Authentication OK")
 
+                #sending tweet
+                api.update_status(msg)
+                
+            except:
+                log("Twitter: Error during authentication")
+                log("Unexpected error:", sys.exc_info()[0])
 
+                #better be careful than get api access blocked again 
+                send_mail("Sending tweet failed, disabling twitter api for now")
+                x_twitter_connection = False
+                
+                
+
+        except FileNotFoundError:
+            log('No twitter token found')
 
 #TWICH
 ############################################################################
@@ -131,7 +170,7 @@ def connect_twtich_irc(old_sock=False):
     if old_sock:
         log("Attempting to reconnect")
         old_sock.close()
-        time.sleep(10)
+        
     while True:
         try:
             #text file. First line is twitch nickname. Second line is oauth token
@@ -159,18 +198,23 @@ def connect_twtich_irc(old_sock=False):
 #EMAIL
 ############################################################################
 def send_mail(msg):
-    with open('email_credetials.txt') as f:
-        lines = f.readlines()
-    
-    log(f'Trying to send mail: {msg}')
-    sender_email = lines[0][:-1]
-    receiver_email = lines[1][:-1]
-    pw = lines[2][:-1]
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(sender_email, pw)
-        server.sendmail(sender_email, receiver_email, msg)
-    log("Mail sent.")
+    try:
+        #read credentials from file
+        with open('email_credetials.txt') as f:
+            lines = f.readlines()
+        log(f'Trying to send mail: {msg}')
+        sender_email = lines[0][:-1]
+        receiver_email = lines[1][:-1]
+        pw = lines[2][:-1]
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(sender_email, pw)
+            server.sendmail(sender_email, receiver_email, msg)
+        log("Mail sent.")
+    except FileNotFoundError:
+        log('No mail credetials')
+    except:
+        log(f'Sending mail "{msg}" failed')
 
 #will return after one stream update message is found
 def handle_raw_message(resp):
@@ -186,19 +230,18 @@ def handle_raw_message(resp):
                 nick = (line.split("!",1)[0])[1:]
                 #full chat log, for debugging only, bloates log file
                 #log(f'{nick}: \t{line.split(":",2)[2]}',False,True)
-                
                 #only look at f3mmbot messages
                 if nick == "f3mmbot":
                     #get message
                     msg = line.split(":",2)[2]
                     #log received f3mmbot messages for debugging
-                    log(f'Found f3mmbot message: "{msg}"')
+                    log(f'Found f3mmbot message: "{msg}"',False,True)
                     #ignore hydration notif etc
-                    if "The next stream will be" in msg:
-                        #cut off formating
-                        return msg[8:-1]
+                    return msg[8:-1]
     return False
+
     
+  
 #write message to terminal and/or log file, also returns log string
 def log(msg, x_terminal=True, x_log=True):
     #timestamp log messages
@@ -270,7 +313,13 @@ def check_filters():
     except FileNotFoundError:
         log('No Filter file found')
         return True
-    
-_init()
+
+
+try:   
+    _init()
+#catch all crash notif
+except:
+    send_mail('Something went wrong, bot crashed')
+    log("Unexpected error:", sys.exc_info()[0])
     
 #Did you actually read the entire code? Hopefully you just scrolled down lol
